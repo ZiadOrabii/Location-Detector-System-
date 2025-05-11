0502d0012ef645d92556ee000b97a7d516baa2ba
Ziad Orabi <113495693+ZiadOrabii@users.noreply.github.com>
2025-05-11 15:53:15 +0300

    Add files via upload

---
+++ Switch/Switch.c
@@ -0,0 +1,22 @@
+#include "TM4C123.h"
+#include "Switch.h"
+#include <string.h>
+#include "SysTick.h"
+
+volatile char displayMode[10] = "Default";  // Global definition
+
+void updateDisplayState(void) {
+    static uint8_t lastState = 1;
+    uint8_t currentState = (GPIOF->DATA & 0x10) >> 4; // SW4 on PF4
+
+    if (lastState == 1 && currentState == 0) { // Falling edge detected (button press)
+        if (strcmp((char *)displayMode, "Default") == 0) {
+            strcpy((char *)displayMode, "Distance");
+        } else {
+            strcpy((char *)displayMode, "Default");
+        }
+        SysTick_Wait1ms(200); // Debounce
+    }
+
+    lastState = currentState;
+}
---
+++ Switch/Switch.h
@@ -0,0 +1,10 @@
+#ifndef SWITCH_H
+#define SWITCH_H
+
+#include <stdint.h>
+
+void updateDisplayState(void);
+extern volatile char displayMode[10];
+
+#endif
+
